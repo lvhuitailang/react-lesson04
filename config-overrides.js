407a41f54a71d91be5efcbec61482a74fbac84f9
wolfie <1039719274@qq.com>
2020-08-07 11:19:34 +0800

    添加less支持

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,12 @@
-const {override} = require('customize-cra');
+const {
+    override,
+    addLessLoader
+} = require('customize-cra');
 
-module.exports = override();
+module.exports = override(
+    addLessLoader({
+        lessOptions: {
+            javascriptEnabled: true,
+        },
+    })
+);
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 
+import './index.less';
 
 
 class App extends Component {
