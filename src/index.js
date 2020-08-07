41391e9f0baf0ab98511230b2bff36da58428857
wolfie <1039719274@qq.com>
2020-08-07 17:23:04 +0800

    添加路由功能进度10%：

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import {render} from "react-dom";
+import {HashRouter as Router,Route,Switch} from 'react-router-dom'
+
+import {mainRouter} from './routes'
+
 
 import App from "./App";
 
 
 render(
-    <App />,
+    <Router>
+        <Switch>
+            <Route path={"/admin"} render={(routerProps)=>{
+                //需要登录才能访问
+                return <App {...routerProps}/>
+            }}/>
+            {
+                mainRouter.map(item => {
+                    return <Route key={item.pathname} path={item.pathname} component={item.component}/>
+                })
+            }
+        })
+        </Switch>
+
+    </Router>,
     document.querySelector('#root')
 );
